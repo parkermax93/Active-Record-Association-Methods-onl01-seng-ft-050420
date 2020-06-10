7102d9422a6b95d9bdd70581aafab05dc72a74be
Parker Williamson <parker.williamson@gmail.com>
2020-06-10 16:55:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # drake = Artist.new(name: "Drake")
-    Artist.find_or_create_by(artist: 'Drake')
+    Artist.find_or_create_by(name: 'Drake')
   end
 end
